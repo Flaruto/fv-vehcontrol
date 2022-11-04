1eb0de538c3d890d707e59070377a53add016366
FlavyV <76876104+Flaruto@users.noreply.github.com>
2022-11-04 07:27:19 -0400

    Initial commit

---
+++ config.lua
@@ -0,0 +1,8 @@
+-- Commands [true/false]
+UseCommands = true
+
+-- Disable ped moving seat from pass to driver unintentionally [true/false]
+DisableSeatShuffle = true
+
+-- Keep vehicle running when exit driver with long F key [ ture/false ]
+LeaveRunning = true
